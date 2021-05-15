1c9a0074737e59caabe16ddc2f054c1990476f5e
Greg Wuller <greg@afofo.com>
2021-05-14 22:22:55 -0700

    monome-snd: multicomponent dai_link configuration for 5.x compat

--- sound/soc/bcm/monome.c
+++ sound/soc/bcm/monome.c
@@ -37,11 +37,10 @@ static int snd_rpi_monome_hw_params(struct snd_pcm_substream *substream,
 			   struct snd_pcm_hw_params *params)
 {
   struct snd_soc_pcm_runtime *rtd = substream->private_data;
-  struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-  struct snd_soc_dai *codec_dai = rtd->codec_dai;
+  struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
+  struct snd_soc_dai *codec_dai = asoc_rtd_to_codec(rtd, 0);
   struct snd_soc_card *card = rtd->card;
 
-  
   int sysclk = 12288000;
   int ret = 0;
 
@@ -53,7 +52,7 @@ static int snd_rpi_monome_hw_params(struct snd_pcm_substream *substream,
       dev_err(card->dev, "Unable to set CS4270 system clock.");
       return ret;
     }
-  
+
   /* cs4270 datasheet says SCLK must be 48x or 64x for max system performance */
   return snd_soc_dai_set_bclk_ratio(cpu_dai, 64);
 }
@@ -72,19 +71,25 @@ static struct snd_soc_ops snd_rpi_monome_ops = {
   .shutdown = snd_rpi_monome_shutdown,
 };
 
+SND_SOC_DAILINK_DEFS(snd_rpi_monome,
+	DAILINK_COMP_ARRAY(COMP_CPU("bcm2708-i2s.0")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("cs4270.1-0048", "cs4270-hifi")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("bcm2708-i2s.0")));
+
 static struct snd_soc_dai_link snd_rpi_monome_dai[] = {
 	{
 		.name = "monome cs4270",
 		.stream_name = "monome cs4270",
-		.cpu_dai_name	= "bcm2708-i2s.0",
-		.codec_dai_name = "cs4270-hifi",
-		.platform_name	= "bcm2708-i2s.0",
-		.codec_name = "cs4270.1-0048",
+		//.cpu_dai_name	= "bcm2708-i2s.0",
+		//.codec_dai_name = "cs4270-hifi",
+		//.platform_name	= "bcm2708-i2s.0",
+		//.codec_name = "cs4270.1-0048",
 		.dai_fmt = SND_SOC_DAIFMT_CBM_CFM | \
 		           SND_SOC_DAIFMT_I2S | \
 		           SND_SOC_DAIFMT_NB_NF,
 		.ops = &snd_rpi_monome_ops,
 		.init = snd_rpi_monome_init,
+		SND_SOC_DAILINK_REG(snd_rpi_monome),
 	},
 };
 
@@ -98,29 +103,37 @@ static struct snd_soc_card snd_rpi_monome = {
 static int snd_rpi_monome_probe(struct platform_device *pdev)
 {
 	int ret = 0;
-	
+
 	snd_rpi_monome.dev = &pdev->dev;
 
 	if (pdev->dev.of_node) {
 		struct device_node *i2s_node;
-		struct snd_soc_dai_link *dai = &snd_rpi_monome_dai[0];
+		//struct snd_soc_dai_link *dai = &snd_rpi_monome_dai[0];
+		struct snd_soc_dai_link_component *cpu_dai = &(snd_rpi_monome_dai[0].cpus[0]);
+		struct snd_soc_dai_link_component *platform_dai = &(snd_rpi_monome_dai[0].platforms[0]);
+
 		i2s_node = of_parse_phandle(pdev->dev.of_node,
 					    "i2s-controller", 0);
 
+		dev_alert(&pdev->dev, "i2s_node = 0x%p\n", i2s_node);
+
 		if (i2s_node) {
-			dai->cpu_dai_name = NULL;
-			dai->cpu_of_node = i2s_node;
-			dai->platform_name = NULL;
-			dai->platform_of_node = i2s_node;
-		}		
+			//dai->cpu_dai_name = NULL;
+			//dai->cpu_of_node = i2s_node;
+			cpu_dai->name = NULL;
+			cpu_dai->of_node = i2s_node;
+			//dai->platform_name = NULL;
+			//dai->platform_of_node = i2s_node;
+			platform_dai->name = NULL;
+			platform_dai->of_node = i2s_node;
+		}
 	}
-	
+
 	ret = snd_soc_register_card(&snd_rpi_monome);
-	if (ret)
-	  {
+	if (ret) {
 	    dev_err(&pdev->dev,
 		    "snd_soc_register_card() failed (%d)\n", ret);
-	  }
+	}
 	return ret;
 }
 
@@ -136,13 +149,13 @@ static const struct of_device_id snd_rpi_monome_of_match[] = {
 MODULE_DEVICE_TABLE(of, snd_rpi_monome_of_match);
 
 static struct platform_driver snd_rpi_monome_driver = {
-       .driver         = {
-		.name   = "snd-rpi-monome",
-		.owner  = THIS_MODULE,
+    .driver = {
+		.name = "snd-rpi-monome",
+		.owner = THIS_MODULE,
 		.of_match_table = snd_rpi_monome_of_match,
-       },
-       .probe          = snd_rpi_monome_probe,
-       .remove         = snd_rpi_monome_remove,
+	},
+	.probe = snd_rpi_monome_probe,
+	.remove = snd_rpi_monome_remove,
 };
 module_platform_driver(snd_rpi_monome_driver);
 
