97c48b89c934c251fce0ac8f890d00d1fc1e593a
Greg Wuller <greg@afofo.com>
2022-02-06 22:35:55 -0800

    build-kernel: allow building multiple configs

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,11 +1,55 @@
 #!/usr/bin/env bash
-set -x -e
 
-# Make sure we have the latest version of the image
-docker pull ghcr.io/monome/norns-kernel-builder/image:latest
+CONFIGS=()
+PULL_IMAGE=YES
+BUILDER_IMAGE_NAME="ghcr.io/monome/norns-kernel-builder/image"
+BUILDER_IMAGE_TAG="latest"
 
-# run build
-docker run --rm \
-  -v "${PWD}":/workdir \
-  -e DEFCONFIG=norns_defconfig \
-  ghcr.io/monome/norns-kernel-builder/image:latest
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --skip-pull)
+      PULL_IMAGE=NO
+      shift # past argument
+      ;;
+    --image-tag)
+      BUILDER_IMAGE_TAG="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --image-name)
+      BUILDER_IMAGE_NAME="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      CONFIGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+if [[ -z "${CONFIGS}" ]]; then
+  CONFIGS+=("norns")
+fi
+
+echo "Building configs: ${CONFIGS}"
+
+set -e
+
+BUILDER_IMAGE="${BUILDER_IMAGE_NAME}:${BUILDER_IMAGE_TAG}"
+if [[ ${PULL_IMAGE} == YES ]]; then
+  CMD="docker pull ${BUILDER_IMAGE}"
+  echo $CMD; ${CMD}
+fi
+
+for config in "${CONFIGS[@]}"; do
+  CMD="docker run --rm \
+    -v "${PWD}":/workdir \
+    -e DEFCONFIG=${config}_defconfig \
+    ${BUILDER_IMAGE}"
+  echo $CMD; ${CMD}
+done
